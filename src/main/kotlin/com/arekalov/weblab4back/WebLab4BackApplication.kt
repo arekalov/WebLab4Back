2c616e256b59969b62f7a165fe68899c92aca3d2
arekalov <artyom.rekalov@gmail.com>
2024-11-17 20:55:29 +0300

    initial commit

---
+++ src/main/kotlin/com/arekalov/weblab4back/WebLab4BackApplication.kt
@@ -0,0 +1,11 @@
+package com.arekalov.weblab4back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebLab4BackApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebLab4BackApplication>(*args)
+}
